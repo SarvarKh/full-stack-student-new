24dd503d8d93f8f96a7e0a32452fe0f8f75fd738
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-08-27 23:04:03 +0500

    Add Table component

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { getAllStudents } from "./client";
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Table } from 'antd';
 import {
     DesktopOutlined,
     PieChartOutlined,
@@ -13,6 +13,29 @@ import './App.css';
 
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
+
+const columns = [
+    {
+        title: 'Id',
+        dataIndex: 'id',
+        key: 'id',
+    },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    },
+    {
+        title: 'Gender',
+        dataIndex: 'gender',
+        key: 'gender',
+    },
+];
 
 function App() {
     const [students, setStudents] = useState([]);
@@ -31,8 +54,11 @@ function App() {
         fetchStudents();
     }, []);
 
-    if (students.length <= 0) {
-        return "no data";
+    const renderStudents = () => {
+        if (students.length <= 0) {
+            return "no data available";
+        }
+        return <Table dataSource={students} columns={columns} />;
     }
 
     return <Layout style={{ minHeight: '100vh' }}>
@@ -68,7 +94,7 @@ function App() {
                     <Breadcrumb.Item>Bill</Breadcrumb.Item>
                 </Breadcrumb>
                 <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                    Bill is a cat.
+                    {renderStudents()}
                 </div>
             </Content>
             <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
