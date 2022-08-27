8a6c3d7e1e247da290563b97fdeef921906b395e
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-08-27 19:02:16 +0500

    Add client.js to make fetch request

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { Button, Radio } from "antd";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type="primary">Hello</Button>
+        <Radio.Group value='small'>
+            <Radio.Button value="large">Large</Radio.Button>
+            <Radio.Button value="default">Default</Radio.Button>
+            <Radio.Button value="small">Small</Radio.Button>
+        </Radio.Group>
     </div>
   );
 }
---
+++ src/frontend/src/client.js
@@ -0,0 +1,15 @@
+import fetch from 'unfetch';
+
+const checkStatus = response => {
+    if (response.ok) {
+        return response;
+    }
+    // convert non-2xx HTTP responses into errors:
+    const error = new Error(response.statusText);
+    error.response = response;
+    return Promise.reject(error);
+}
+
+export const getAllStudents = () =>
+    fetch("http://localhost:8080/api/v1/students")
+        .then(checkStatus);
