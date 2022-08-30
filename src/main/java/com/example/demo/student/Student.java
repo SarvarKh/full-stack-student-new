907b8e3ccf9a3d1cc5808038e7e6debb53108525
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-08-30 14:32:28 +0500

    Add & implement Spring validation dependencey

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -3,6 +3,9 @@ package com.example.demo.student;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @ToString
 @Getter
@@ -23,8 +26,11 @@ public class Student {
             generator = "student_sequence",
             strategy = GenerationType.SEQUENCE)
     private Long id;
+    @NotBlank
     private String name;
+    @Email
     private String email;
+    @NotNull
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,7 +20,7 @@ public class StudentController {
     }
 
     @PostMapping
-    public void addStudent(@RequestBody Student student) {
+    public void addStudent(@Valid @RequestBody Student student) {
         studentService.addStudent(student);
     }
 
