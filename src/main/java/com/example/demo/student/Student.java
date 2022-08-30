c6ec3919724eab2eac239019c90851e443e3ece5
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-08-30 14:40:59 +0500

    Add db contraints and add-btn when data is empty

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -27,11 +27,14 @@ public class Student {
             strategy = GenerationType.SEQUENCE)
     private Long id;
     @NotBlank
+    @Column(nullable = false)
     private String name;
     @Email
+    @Column(nullable = false, unique = true)
     private String email;
     @NotNull
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Gender gender;
 
     public Student(String name, String email, Gender gender) {
